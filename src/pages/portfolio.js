a2fa1bf8b810912042f30333048752ef46fbe93d
David Figueroa <dfigue92@gmail.com>
2018-12-10 12:32:31 -0700

    Began including portfolio content

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Link } from "react-router-dom";
+import Parser from 'rss-parser';
 
 import { KeystoneOutline } from '../assets/customSVGs';
 import Menu, { MenuItem } from '../components/misc/animatedMenu';
@@ -24,7 +25,8 @@ export default class Portfolio extends Component {
         keystoneSelection: 'unselected',
         selection: 'unselected',
         menuSelected: false,
-        selectedItem: 'CODE.'
+        selectedItem: 'CODE.',
+        mediumFeed: []
     }
   
     switchContainer = () => {
@@ -36,13 +38,93 @@ export default class Portfolio extends Component {
       }
     }
 
-    switchMenuItem = (item) => {
-        this.setState({ selectedItem: item })
-        console.log(item.key)
+    getMediumFeed = async () => {
+        let parser = new Parser();
+        let feed = await parser.parseURL('https://cors-anywhere.herokuapp.com/https://medium.com/feed/@David_Figueroa').then(res => {
+            let entries = [];
+            res.items.forEach((item, index) => {
+                if (item.categories) {
+                    console.log(item.categories)
+                    entries.push(item)
+                }
+            })
+            let sliceEntries = entries.slice(0, 5)
+            return sliceEntries
+        })
+        let mediumFeed = feed.map(entry => {
+            let entryObj = {};
+            entryObj['title'] = entry.title;
+            entryObj['link'] = entry.link;
+            entryObj['date'] = entry.isoDate;
+            return(entryObj)
+        })
+        console.log(feed)
+        this.setState({ mediumFeed: mediumFeed })
+    }
+
+    setContent = (item) => {
+        if (item === 'CODE.') {
+            console.log('coding')
+            return(
+                <div 
+                    style={{
+                        display: 'grid',
+                        gridTemplate: '1fr / 1fr .3fr'
+                    }}
+                >
+                    <div 
+                        style={{ 
+                            gridArea: '1 / 1 / 1 / 1' ,
+                            display: 'flex',
+                            flexDirection: 'column'
+                        }}
+                    >
+                        <div style={{flexDirection: 'column'}}>
+                            <div>
+                                <text className='contentTitle' style={{fontSize: '1.5em'}}>{'PROJECTS & EXPERIENCE'}</text>
+                            </div>
+                            <div className='content' style={{flexDirection: 'column', marginTop: '.5em'}}>
+                                <text>
+                                    FULL-STACK DEVELOPER
+                                    <br/>
+                                    Keystone - Life Analytics
+                                    <ul>
+                                        <li>Managed project through entire production cycle</li>
+                                        <li>Responsible for UI/UX design, React Native development, and database architecture</li>
+                                        <li>Created, published, and maintained product website</li>
+                                    </ul>
+                                </text>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='contentTitle' style={{ gridArea: '1 / 2 / 1 / 2' }}>
+                        <text style={{fontSize: '1.5em'}}>SKILLS</text>
+                    </div>
+                </div>
+            )
+        } else if (item === 'WRITE.') {
+            return(
+                <div style={{ flexDirection: 'column' }}>
+                    {
+                        this.state.mediumFeed.map((item, index) => (
+                            <div style={{ width: '100%', height: '5em', marginTop: (index !== 0 ? '.25em' : null) }}>
+                                <a href={item.link}>
+                                    <text style={{fontSize: '1.3em', textDecoration: 'none'}}>
+                                        {item.title}
+                                    </text>
+                                </a>
+                            </div>
+                        ))
+                    }
+                </div>
+            )
+        } else if (item === 'PLAY.') {
+
+        }
     }
 
     componentDidMount() {
-        console.log(this.state.selectedItem)
+        this.getMediumFeed()
     }
   
     render() {
@@ -80,7 +162,9 @@ export default class Portfolio extends Component {
                         {
                             portfolioMenu.map((item) => (
                                 <MenuItem 
-                                    onClick={() => this.setState({ selectedItem: item.key })}
+                                    onClick={() => {
+                                        this.setState({ selectedItem: item.key })
+                                    }}
                                     backgroundColor={item.key === this.state.selectedItem ? globalStyle.colors.electricBlue : null}
                                     style={{ 
                                         cursor: 'pointer'
@@ -98,12 +182,12 @@ export default class Portfolio extends Component {
               </PageInnerContainer>
 
               <div 
-                className='content'
                 style={{ display: 'grid', gridTemplate: 'repeat(12, 1fr) / repeat(12, 1fr)', height: '100%', width: '100%'}}
               >
 
               {/* CONTENT QUOTE HEADER */}
                 <div 
+                    className='content'
                     style={{
                         gridArea: '4 / 3 / 8 / 5', 
                         display: 'grid', 
@@ -147,9 +231,9 @@ export default class Portfolio extends Component {
                         padding: '1em',
                     }}
                 >
-                    <text style={{fontSize: '1em', }}>
-                        Experience working with React, React Native, JavaScript, Python, and tons of other important tech stuff.
-                    </text>
+                    {
+                        this.setContent(this.state.selectedItem)
+                    }
                 </div>
               </div>
             </MainContainer>
@@ -168,4 +252,4 @@ export default class Portfolio extends Component {
         </div>
       );
     }
-  }
+}
